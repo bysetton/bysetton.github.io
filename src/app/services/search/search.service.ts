86568c2e15778bca924badb5c8642e2b91885468
bysetton <barak@bysetton.com>
2020-07-04 00:07:08 +0200

    first deploy: Fix circular dependencies

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,11 +1,9 @@
 import { Injectable, OnInit } from '@angular/core';
-
-import { BehaviorSubject, Observable, of } from 'rxjs';
-
-import { Company } from '../../interfaces/company.interface'
-import { Tag } from '../../interfaces/company.interface'
-import { CompaniesService } from '../../services/companies/companies.service'
 import { Router, NavigationEnd } from '@angular/router';
+
+import { BehaviorSubject } from 'rxjs';
+
+import { Tag } from '../../interfaces/company.interface'
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +13,8 @@ export class SearchService {
   private _isHomePage: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private activeTags: BehaviorSubject<Tag[]> = new BehaviorSubject<Tag[]>([]);
   private _activeTags: Tag[] = [];
-  // private searchValue: string;
-  // private _searchResults = new BehaviorSubject<Company[]>([]);
-  // private companies = this.companiesService.companies;
-  // private results: Company[];
-  // private companyNameSearchResult: Company[];
-  // private tagSearchResult: Company[];
-  // private aboutSearchResult: Company[];
-  // private currentPage: Page;
 
-  constructor(private companiesService: CompaniesService, private router: Router) {
+  constructor(private router: Router) {
     this.router.events.subscribe((event: NavigationEnd) => {
       if (event.urlAfterRedirects == '/home') {
         this._isHomePage.next(true);
@@ -67,65 +57,6 @@ export class SearchService {
   buildUrl(searchValue: string): void {
 
   }
-
-  // get searchResults(): Observable<Company[]> {
-  //   return this._searchResults.asObservable();
-  // }
-
-  // setSearchResults(results: Company[]): void {
-  //   this.results = results;
-  //   this._searchResults.next(this.results);
-  // }
-
-
-  // searchWithValue(value: string) {
-  //   this.searchActiveTags();
-  //   if (value) {
-  //     this.searchValue = value;
-  //     this.searchCompanyName(this.searchValue);
-  //     this.searchAbout(this.searchValue);
-  //   }
-  //   this.combineSearchResults();
-  //   this._searchResults.next(this.results);
-  // }
-
-  // private searchActiveTags(): void {
-  //   if (this.ActiveTags.length == 0) {
-  //     return;
-  //   }
-  //   this.tagSearchResult = this.results.filter((company) => {
-  //     return company.tags.some((tag) => {
-  //       return this.ActiveTags.some((t) => {
-  //         return t.name == tag.name;
-  //       });
-  //     });
-  //   });
-  // }
-
-  // private searchCompanyName(value: string): void {
-  //   if (!value) {
-  //     this.results = this.companies;
-  //     return;
-  //   }
-  //   this.companyNameSearchResult = this.companies.filter((company) => {
-  //     return company.name && company.name.toLowerCase().includes(value.toLowerCase());
-  //   });
-  // }
-
-  // private searchAbout(value: string): void {
-  //   if (!value) {
-  //     this.results = this.companies;
-  //     return;
-  //   }
-  //   this.companyNameSearchResult = this.companies.filter((company: Company) => {
-  //     return company.about && company.about.toLowerCase().includes(value.toLowerCase());
-  //   });
-  // }
-
-  // private updateSearchOnTags(tags: Tag[]): void {
-  //   this.searchWithValue(this.searchValue);
-  //   this._tags.next(tags);
-  // }
 }
 
 enum Page {
